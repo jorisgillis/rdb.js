ea4c8ae14a81a94931a263b3f7dafa72ee2314bf
Joris Gillis <j.gillis@aca-it.be>
2013-12-30 15:35:55 +0100

    Wrapper
    Extended get method

--- db/db.js
+++ db/db.js
@@ -1,4 +1,5 @@
 var sqlite = require("sqlite3");
+var _ = require("underscore");
 
 var dbFile = "database.db";
 
@@ -6,8 +7,37 @@ function createDB() {
     return new sqlite.Database(dbFile);
 }
 
-exports.getAllRecipes = function() {
+exports.getAllRecipesWithTypesAndFirstPhoto = function() {
     var db = createDB();
 
-    db.all("SELECT ");
+    var listOfRecipes = [];
+    
+    db.serialize(function() {
+        db.each("SELECT * FROM recipes ORDER BY creation_date", function(err, recipe) {
+            listOfRecipes.push(recipe);
+            
+            var typesStmt = db.prepare("SELECT type FROM recipe_type WHERE recipe_id=?");
+            typesStmt.all(recipe.id, function(err, rows) {
+                recipe.types = _.map(rows, extractType);
+            });
+            typesStmt.finalize();
+
+            var photoStmt = db.prepare("SELECT photos.name, photos.photo "+
+                                       "FROM recipe_photo, photos"+
+                                       "WHERE recipe_photo.recipe_id=? "+
+                                       "AND photos.id=recipe_photo.photo_id");
+            photoStmt.get(recipe.id, function(err, photo) {
+                recipe.photo = photo;
+            });
+            photoStmt.finalize();
+        });
+    });
+
+    db.close();
+    return listOfRecipes;
 };
+
+
+function extractType(typeObj) {
+    return typeObj.type
+}
