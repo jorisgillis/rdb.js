c88afabd83f6d4e26420423dc2b099b74b25ba51
Joris Gillis <j.gillis@aca-it.be>
2014-02-24 17:48:05 +0100

    Routing
    Not yet working, duplication of paths in Angular and Express, probably
    best to place all JSON calls behind an '/api/' prefix, to differentiate
    between routing in the web app and REST calls.

--- public/app.angular.js
+++ public/app.angular.js
@@ -2,8 +2,20 @@
 
 var rdb = angular.module('rdb', ['ngResource', 'ngRoute']);
 
+rdb.config(function($routeProvider) {
+    // Recipe
+    $routeProvider.when('/recipe/:recipeId', 
+        {
+            templateUrl: 'recipe.html',
+            controller: 'RecipeController'
+        });
+
+    // Otherwise: go to index
+    $routeProvider.otherwise({redirectoTo: '/'});
+});
+
 rdb.controller('rdbController', 
-    ['$scope', '$routeProvider', 'Recipes',
+    ['$scope', 'Recipes',
         function($scope, Recipes) {
             var allRecipes = Recipes.all().$promise;
             allRecipes.then(
