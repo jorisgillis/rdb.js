9d75bd0d69f19ba3bc8d0e2f7a07445c2043123b
Joris Gillis <joris.gillis@telenet.be>
2014-03-30 13:13:04 +0200

    Formatting

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
 var express = require('express'),
-    routes = require('./routes'),
-    passport = require('passport'),
-    GoogleStrategy = require('passport-google').Strategy,
-    db = require('./models');
-    path = require('path');
+routes = require('./routes'),
+passport = require('passport'),
+GoogleStrategy = require('passport-google').Strategy,
+db = require('./models');
+path = require('path');
 
 var app = express();
 var url = process.env.HOSTNAME || 'localhost';
 var port = process.env.PORT || '3000';
 app.configure(function(){
-    app.set('views', __dirname + '/views');
-    app.set('view engine', 'jade');
-    app.use(express.cookieParser())
-    app.use(express.session({secret: 'test'}));
-    app.use(passport.initialize());
-    app.use(passport.session());
-    app.use(express.static(path.join(__dirname, 'public')));
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.cookieParser())
+  app.use(express.session({secret: 'test'}));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(express.static(path.join(__dirname, 'public')));
 });
 
 passport.serializeUser(function(user, done){
-    done(null, user);
+  done(null, user);
 });
 
 passport.deserializeUser(function(id, done){
-    done(null, id);
+  done(null, id);
 });
 
 passport.use(new GoogleStrategy({
-        returnURL: 'http://' + url + ':' + port + '/auth/google/return',
-        realm: 'http://' + url + ':' + port
-    },
-    function(identifier, profile, done) {
-        done(null, profile.emails[0].value);
-    }
+  returnURL: 'http://' + url + ':' + port + '/auth/google/return',
+  realm: 'http://' + url + ':' + port
+},
+function(identifier, profile, done) {
+  done(null, profile.emails[0].value);
+}
 ));
 
 // Index
@@ -41,10 +41,10 @@ app.get('/', routes.index);
 // Auth URLs
 app.get('/auth/google', passport.authenticate('google'));
 app.get('/auth/google/return',
-    passport.authenticate('google', {failureRedirect: '/'}),
-    function(req, res){
-        res.redirect('/');
-    });
+passport.authenticate('google', {failureRedirect: '/'}),
+function(req, res){
+  res.redirect('/');
+});
 app.get('/logout', routes.logout);
 
 // REST Interface
@@ -59,12 +59,12 @@ app.get('*', routes.index);
 
 // Setting up database and starting application
 db
-    .sequelize
-    .sync({ force: false })
-    .complete(function(err) {
-        if (err) {
-            throw err
-        } else {
-            app.listen(port);
-        }
-    });
+.sequelize
+.sync({ force: false })
+.complete(function(err) {
+  if (err) {
+    throw err
+  } else {
+    app.listen(port);
+  }
+});
--- routes/index.js
+++ routes/index.js
@@ -3,62 +3,75 @@ var _ = require('../node_modules/lodash/lodash.js');
 
 //-- Regular HTTP requests
 exports.index = function(req, res) {
-    if(typeof req.user === 'undefined'){
-        res.render('login');
-    } else {
-        res.render('index');
-    }
+  if(typeof req.user === 'undefined'){
+    res.render('login');
+  } else {
+    res.render('index');
+  }
 };
 
 exports.logout = function(req, res) {
-    req.logout();
-    res.redirect('/');
+  req.logout();
+  res.redirect('/');
 }
 
 
 //-- REST API
 exports.overview = function(req, res) {
-    db.Recipe.findAll().success(
-        function(recipes) {
-            res.json({'recipes': recipes});
-        }
-    );
+  db.Recipe.findAll().success(
+    function(recipes) {
+      res.json({'recipes': recipes});
+    }
+  );
 }
 
 exports.recipe = function(req, res) {
-    var recipeId = req.param('recipeId');
-    db.Recipe.find({where: {id: recipeId}, include: [db.Ingredient, db.Photo, db.Type, db.Season]}).success(
-        function(recipe) {
-            db.Quantity.findAll({where: {recipeId: recipeId}})
-            .then(function(rawQuantities) {
-                var quantities =  _.map(rawQuantities, function(rawQuantity) { return rawQuantity.dataValues; });
-                var ingredientIds = _.map(quantities, function(quantity) { return quantity.IngredientId; });
-                db.Ingredient.findAll({where: { id: ingredientIds } }).success(function(ingredients) {
-                    var ingredientList = 
-                        _.map(
-                            _.zip(
-                                _.map(quantities, function(quantity) { return quantity.quantity; }), 
-                                _.map(ingredients, function(ingredient) { return ingredient.dataValues; })),
-                            function(pair) {
-                                return {'quantity': pair[0], 'ingredient': pair[1]};
-                            }
-                        );
-                    res.json({'recipe': recipe, 'ingredientList': ingredientList});
-                });
+  var recipeId = req.param('recipeId');
+  db.Recipe.find({where: {id: recipeId}, include: [db.Ingredient, db.Photo, db.Type, db.Season]}).success(
+    function(recipe) {
+      db.Quantity.findAll({where: {recipeId: recipeId}})
+      .then(function(rawQuantities) {
+        var quantities =  _.map(rawQuantities,
+          function(rawQuantity) {
+            return rawQuantity.dataValues;
+          });
+          var ingredientIds = _.map(quantities,
+            function(quantity) {
+              return quantity.IngredientId;
             });
-        }
-    );
-}
+          db.Ingredient.findAll({where: { id: ingredientIds } }).success(function(ingredients) {
+            var ingredientList =
+            _.map(
+              _.zip(
+                _.map(quantities,
+                  function(quantity) {
+                    return quantity.quantity;
+                  }),
+                  _.map(ingredients,
+                    function(ingredient) {
+                      return ingredient.dataValues;
+                    })
+                  ),
+                  function(pair) {
+                    return {'quantity': pair[0], 'ingredient': pair[1]};
+                  }
+                );
+                res.json({'recipe': recipe, 'ingredientList': ingredientList});
+              });
+            });
+          }
+        );
+      }
 
-exports.ingredient = function(req, res) {
-    var ingredientId = req.param('ingredientId');
-    db.Ingredient.find({where: {id: ingredientId}}).then(function(ingredient) {
-        res.json({'ingredient': ingredient});
-    });
-}
+      exports.ingredient = function(req, res) {
+        var ingredientId = req.param('ingredientId');
+        db.Ingredient.find({where: {id: ingredientId}}).then(function(ingredient) {
+          res.json({'ingredient': ingredient});
+        });
+      }
 
-exports.ingredients = function(req, res) {
-    db.Ingredient.findAll().then(function(ingredients) {
-        res.json({'ingredients': ingredients})
-    });
-}
+      exports.ingredients = function(req, res) {
+        db.Ingredient.findAll().then(function(ingredients) {
+          res.json({'ingredients': ingredients})
+        });
+      }
